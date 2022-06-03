c0a2ae82e96614918698582200f3652d30dfc67d
Zainurin Zainal <zainurin.syafiq@globaltium.com>
2022-06-03 17:44:56 +0800

    fix: url auth

--- routes/custom-auth.php
+++ routes/custom-auth.php
@@ -5,18 +5,20 @@ use Zaizainal\CustomAuth\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
-    Route::middleware('guest')->group(function () {
-        Route::get('register', [RegisterController::class, 'create'])->name('register');
-        Route::post('register', [RegisterController::class, 'store'])->name('register.attempt');
+    Route::group(['prefix' => 'zaizainal'], function () {
+        Route::middleware('guest')->group(function () {
+            Route::get('register', [RegisterController::class, 'create'])->name('register');
+            Route::post('register', [RegisterController::class, 'store'])->name('register.attempt');
 
-        Route::get('login', [LoginController::class, 'show'])->name('show.login');
-        Route::post('login', [LoginController::class, 'login'])->name('login.attempt');
-    });
-    Route::middleware('auth')->group(function () {
-        Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+            Route::get('login', [LoginController::class, 'show'])->name('show.login');
+            Route::post('login', [LoginController::class, 'login'])->name('login.attempt');
+        });
+        Route::middleware('auth')->group(function () {
+            Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
-        Route::get('home', function () {
-            return view('custom-auth::home');
-        })->name('home');
+            Route::get('home', function () {
+                return view('custom-auth::home');
+            })->name('home');
+        });
     });
 });
